1efcbae2f813fe9ea9cbc2027b7cc4f1f637576c
LoreBadTime <concas324@gmail.com>
2022-08-17 14:41:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,6 +20,7 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
+  'Base_Converter.html'
 ];
 
 // The install handler takes care of precaching the resources we always need.
